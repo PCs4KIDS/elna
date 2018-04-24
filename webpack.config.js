d469460f1f986fe309dcace629fd967905d64a96
Samuel Imolorhe <samuel@sir-muels-mbp.lan>
2018-04-25 00:18:27 +0200

    Added initial game assets.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: 'game.js'
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        inline: true,
+        publicPath: '/build/'
+    },
+
+    module: {
+        rules: [
+          {
+            test: [ /\.vert$/, /\.frag$/ ],
+            use: 'raw-loader'
+          }
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
+    ]
+
+};
